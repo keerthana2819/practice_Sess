29d3ccce0392e972d544a7fe04e52a0e272e8580
keerthana2819 <mkeerthana1928@gmail.com>
2024-11-05 10:46:35 -0600

    added new file

---
+++ change password.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_password(length=5):
+    #length changed from 6 to 5
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+    length = int(input("Enter password length: "))
+    print("Generated Password:", generate_password(length))
+
+if __name__ == "__main__":
+    main()
+    
